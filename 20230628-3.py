297d5282f300cb4d7c1ca4ee6257b46949f85527
wooyeol0420 <137777772+wooyeol0420@users.noreply.github.com>
2023-06-28 18:00:20 +0900

    Add files via upload

---
+++ 20230628-3.py
@@ -0,0 +1,18 @@
+class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second    
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    
+class MoreFourCal(FourCal):
+    def pow(self):
+        result = self.first ** self.second
+        return result
+
+a = MoreFourCal(4, 2)
+print(a.pow())
