74d7a1fb0748a4223e8a2f4b06e608b4f111d84d
wooyeol0420 <137777772+wooyeol0420@users.noreply.github.com>
2023-07-04 20:34:55 +0900

    Add files via upload

---
+++ 20230704main.js
@@ -0,0 +1,11 @@
+let target = document.querySelector(#"dynamic");
+let stringArr = ["Learn to HTML","Learn to CSS","Learn to JavaScript","Learn to Python"];
+let queryString = stringArr[Math.random() * stringArr.length];
+let selectStringArr = selecString.split("";)
+
+console.log(selectString);
+
+function blink(){
+    target.classList.toggle("active");
+}
+setInterval(blink, 500);
